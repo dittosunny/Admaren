770fab734f11cd885749390b27147718610a631b
Ditto sunny <dittosunny95@gmail.com>
2024-06-06 21:56:23 +0530

    view selected

--- src/app/sprint-calculator/sprint-calculator.component.ts
+++ src/app/sprint-calculator/sprint-calculator.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
-import {Validators} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { ServiceService } from '../service.service';
-
-
 
 @Component({
   selector: 'app-sprint-calculator',
   templateUrl: './sprint-calculator.component.html',
-  styleUrls: ['./sprint-calculator.component.css']
+  styleUrls: ['./sprint-calculator.component.css'],
 })
 export class SprintCalculatorComponent {
   stories!: any[];
-  id!:number
+  id!: number;
 
-  constructor(private formBuilder: FormBuilder , private dataService: ServiceService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: ServiceService
+  ) {}
 
   sprintform = this.formBuilder.group({
     sprintpoint: ['', Validators.required],
@@ -24,7 +25,7 @@ export class SprintCalculatorComponent {
     this.dataService.getStories().subscribe(
       (data: any) => {
         console.log(data);
-        this.stories = data
+        this.stories = data;
       },
       (error: any) => {
         console.error('Error fetching stories:', error);
@@ -37,12 +38,13 @@ export class SprintCalculatorComponent {
       console.log('Form is invalid');
     } else {
       const sprintPoints = this.sprintform.value.sprintpoint;
+      console.log(sprintPoints);
       
-      const selectedStories = this.stories.filter(story => story.storyPoint === sprintPoints);
-      
+      const selectedStories = this.stories.filter(
+        (story) => story.storyPoint === sprintPoints
+      );
+
       console.log('Selected Stories:', selectedStories);
-  
-     
     }
   }
 
@@ -51,18 +53,18 @@ export class SprintCalculatorComponent {
       console.log('No stories available');
       return;
     }
-  
-    const indexToDelete = this.stories.findIndex(story => story.storyPoint === storyPointToDelete);
-  
+
+    const indexToDelete = this.stories.findIndex(
+      (story) => story.storyPoint === storyPointToDelete
+    );
+
     if (indexToDelete !== -1) {
       this.stories.splice(indexToDelete, 1);
-      console.log(`Story with story point ${storyPointToDelete} deleted successfully.`);
+      console.log(
+        `Story with story point ${storyPointToDelete} deleted successfully.`
+      );
     } else {
       console.log(`No story found with story point ${storyPointToDelete}.`);
     }
   }
-  
-  
-  
-  
 }
